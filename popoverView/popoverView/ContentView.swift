0a58f6b78fa6bd8189e38a7aa295d81775fbc336
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2024-04-15 23:50:55 -0400

    Popover in SwiftUI

--- popoverView/popoverView/ContentView.swift
+++ popoverView/popoverView/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var displayPopover = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        Button("Display Popover", systemImage: "hand.tap.fill") {
+            displayPopover = true
         }
-        .padding()
+        .font(.title)
+        .foregroundStyle(.orange)
+        .popover(isPresented: $displayPopover) {
+            Text("Hello World")
+                .font(.largeTitle)
+                .fontWeight(.ultraLight)
+                .foregroundStyle(.purple)
+                .underline()
+            Text("Display your content here")
+                .font(.footnote)
+                .foregroundStyle(.teal)
+        }
     }
 }
 
