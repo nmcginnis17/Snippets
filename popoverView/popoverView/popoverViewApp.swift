5264e8553a1e663a44accd1908623f5fc90a7927
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2024-04-15 23:32:42 -0400

    popover initial commit

---
+++ popoverView/popoverView/popoverViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  popoverViewApp.swift
+//  popoverView
+//
+//  Created by Nicholas McGinnis on 4/15/24.
+//
+
+import SwiftUI
+
+@main
+struct popoverViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
