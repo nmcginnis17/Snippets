fd10004a9834726a3ac09672818f20f3ebae0f33
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2024-04-18 23:41:08 -0400

    Initial commit Alt Light/Dark Mode

---
+++ showAltOnLightVsDarkMode/showAltOnLightVsDarkMode/showAltOnLightVsDarkModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  showAltOnLightVsDarkModeApp.swift
+//  showAltOnLightVsDarkMode
+//
+//  Created by Nicholas McGinnis on 4/18/24.
+//
+
+import SwiftUI
+
+@main
+struct showAltOnLightVsDarkModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
