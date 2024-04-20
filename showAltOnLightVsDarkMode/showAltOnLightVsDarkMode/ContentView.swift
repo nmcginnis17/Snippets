4ee2d1e437918b0b1ede5e16a88842330fcc2799
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2024-04-19 21:08:00 -0400

    Light/Dark Mode

--- showAltOnLightVsDarkMode/showAltOnLightVsDarkMode/ContentView.swift
+++ showAltOnLightVsDarkMode/showAltOnLightVsDarkMode/ContentView.swift
@@ -7,15 +7,45 @@
 
 import SwiftUI
 
+struct AdaptiveView<D: View, L: View>: View {
+    @Environment(\.colorScheme) var colorScheme
+    let dark: D
+    let light: L
+    
+    init(dark: D, light: L) {
+        self.dark = dark
+        self.light = light
+    }
+    
+    init(dark: () -> D, light: () -> L) {
+        self.dark = dark()
+        self.light = light()
+    }
+    
+    @ViewBuilder var body: some View {
+        if colorScheme == .dark {
+            dark
+        } else {
+            light
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            AdaptiveView {
+                VStack {
+                    Text("Dark Mode")
+                    Image(systemName: "moon")
+                }
+            } light: {
+                VStack {
+                    Text("Light Mode")
+                    Image(systemName: "sun")
+                }
+            }
         }
-        .padding()
     }
 }
 
