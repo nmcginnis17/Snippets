9cb3aba230278e4eae0d117a6d34912429409c65
Nick McGinnis <nmcginnis17@users.noreply.github.com>
2024-04-28 12:48:51 -0400

    let users import videos using PhotosPicker

---
+++ letUsersImportVideosWithPhotosPicker/letUsersImportVideosWithPhotosPicker/letUsersImportVideosWithPhotosPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  letUsersImportVideosWithPhotosPickerApp.swift
+//  letUsersImportVideosWithPhotosPicker
+//
+//  Created by Nicholas McGinnis on 4/28/24.
+//
+
+import SwiftUI
+
+@main
+struct letUsersImportVideosWithPhotosPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
